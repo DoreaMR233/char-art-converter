a36edd08689e82d347ed873d4e83f6df4adec96d
DoreaMR233 <ruirui2305178@126.com>
2025-06-23 00:55:59 +0800

    添加WebpProcessResult模型类以处理WebP结果

---
+++ backend/src/main/java/com/doreamr233/charartconverter/model/WebpProcessResult.java
@@ -0,0 +1,31 @@
+package com.doreamr233.charartconverter.model;
+
+import lombok.Data;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * WebP处理结果类
+ */
+@Data
+public class WebpProcessResult {
+    private final int frameCount;
+    private final int[] delays;
+    private final BufferedImage[] frames;
+    private String oriTaskId;
+    private String webPTaskId;
+
+    public WebpProcessResult(int frameCount, int[] delays, BufferedImage[] frames) {
+        this.frameCount = frameCount;
+        this.delays = delays;
+        this.frames = frames;
+    }
+
+    public WebpProcessResult(int frameCount, int[] delays, BufferedImage[] frames, String oriTaskId, String webPTaskId) {
+        this.frameCount = frameCount;
+        this.delays = delays;
+        this.frames = frames;
+        this.oriTaskId = oriTaskId;
+        this.webPTaskId = webPTaskId;
+    }
+}
